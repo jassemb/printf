2ea2f4ff32bd5e57b56d4294b279d0f1c467828e
jassemb <benalijassem201@gmail.com>
2021-07-27 12:45:22 -0700

    lib file

---
+++ holberton.h
@@ -0,0 +1,37 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <limits.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * struct printf - struct printf
+ * @c: The character
+ * @fun: The function associated
+ */
+typedef struct printf
+{
+	char c;
+	int (*fun)(va_list);
+} printf_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int get_func(const char s, va_list ap);
+
+
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_percent(va_list arg);
+int print_int(va_list arg);
+int print_binaire(va_list arg);
+int print_unsf(va_list arg);
+int print_oct(va_list arg);
+int print_hex(va_list arg);
+int print_heX(va_list arg);
+int _rot13(va_list ap);
+int _printrev(va_list ap);
+int print_P(va_list p);
+
+#endif/* PRINTF_H */
