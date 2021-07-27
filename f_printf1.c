2bd1ce0f0294f635402eb0806ade968a86afa3c5
jassemb <benalijassem201@gmail.com>
2021-07-27 12:33:53 -0700

    f_printf1 function for the printf

---
+++ f_printf1.c
@@ -0,0 +1,116 @@
+#include "holberton.h"
+/**
+ * print_char - prints a character
+ * @arg: The character to print, argument pointer
+ *
+ * Return: 1
+ */
+int print_char(va_list arg)
+{
+	_putchar(va_arg(arg, int));
+	return (1);
+}
+/**
+ * print_string - prints a string
+ * @arg: The string to print, argument pointer
+ *
+ * Return: i
+ */
+int print_string(va_list arg)
+{
+	char *s = va_arg(arg, char *);
+	int i = 0;
+
+	if (s == NULL)
+	s = "(null)";
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ *print_percent - print percent
+ *@arg: argument of type of va_list
+ * Return: integer
+ */
+int print_percent(va_list arg)
+{
+	(void)arg;
+
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * print_int - function that prints an integer with helper function.
+ * @arg: The string to print, argument pointer
+ *
+ * Return: counter
+ *
+ */
+int print_int(va_list arg)
+{
+	int n = va_arg(arg, int);
+	int count = 0, rest = 1;
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+		count++;
+	}
+
+	while ((x / rest) > 9)
+	{
+		rest = rest * 10;
+	}
+	while (rest > 0)
+	{
+		_putchar(x / rest + '0');
+		x = (x % rest);
+		rest = rest / 10;
+		count++;
+	}
+	return (count);
+}
+/**
+ * print_b - convert an integer to binary (base 2).
+ *
+ * @arg: the list of arguments.
+ *
+ *  Return: number of printed element.
+ */
+int print_b(va_list arg)
+{
+	unsigned int binary[32];
+	int i = 0;
+	unsigned int n = 0, count = 0;
+
+	n = va_arg(arg, unsigned int);
+
+	if (n <= 1)
+	{
+		_putchar(n + '0');
+		count++;
+	}
+	else
+	{
+		while (n > 0)
+		{
+			binary[i] = n % 2;
+			n = n / 2;
+			i++;
+		}
+		for (i = i - 1; i >= 0; i--)
+		{
+			_putchar(binary[i] + '0');
+			count++;
+		}
+	}
+	return (count);
+}
